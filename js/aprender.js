5b135003724fc458af06f88159dcb377802b3a3d
julianromero-dev <romeroortizjulianalejandro@gmail.com>
2025-06-13 09:19:11 -0500

    funciones javascript

---
+++ js/aprender.js
@@ -0,0 +1,43 @@
+console.log ("hola mundo")
+//alert("hola mundo")
+//alert.log("hola mundo")
+// variables en js let, var, const,
+const edad = 18;
+let edad_dos = 18;
+edad_dos = edad_dos + 2;
+console.log(edad_dos)
+//var entero = 20
+//var decimal = 20.5
+var entero = 20
+var decimal = 20.5
+var string = "hola"
+console.log(string)
+var boolean = true
+console.log(boolean)
+var arrays_string = ["lun" , "mar" , "nierco" , "jueves" , "vierne" , "saba" , "domin"]
+console.log(arrays_string)
+
+
+    
+
+//estructura logica
+for (let i = 0; i < arrays_string.length; i++) {
+  console.log(arrays_string[i])
+  Swal.fire("SweetAlert2 is working!");
+}
+
+
+console.log(arrays_string)
+console.log(arrays_string.length)
+var impresion = "";
+for (let i = 0; i < arrays_string.length; i++){
+    console.log(arrays_string[i])
+impresion = impresion + arrays_string[i]
+}
+Swal.fire(impresion)
+
+
+var num1 = 14
+var num2 = 15
+var suma = num1 + num2
+console.log(suma)
