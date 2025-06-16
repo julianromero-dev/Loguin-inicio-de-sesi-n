14e50bb4e2328c034b97c82093a477992f2ae67c
julianromero-dev <romeroortizjulianalejandro@gmail.com>
2025-06-16 11:40:01 -0500

    login-funcional-proyecto

--- javascript/aprender.js
+++ javascript/aprender.js
@@ -1,44 +1,29 @@
-console.log ("hola mundo")
-//alert("hola mundo")
-//alert.log("hola mundo")
-// variables en js let, var, const,
+console.log("hola profe")
+//variables = let,var,const
+
 const edad = 18;
 let edad_dos = 18;
+
 edad_dos = edad_dos + 2;
 console.log(edad_dos)
-//var entero = 20
-//var decimal = 20.5
-var entero = 20
-var decimal = 20.5
-var string = "hola"
-console.log(string)
-var boolean = true
+
+//tipos de datos = entero decimal , string , arrays
+
+var entero = 20;
+var decimal = 20.5;
+var string = "mi nombre es spike"
+var boolean = true //false
 console.log(boolean)
-var arrays_string = ["lun" , "mar" , "nierco" , "jueves" , "vierne" , "saba" , "domin"]
+var arrays_string = ["lun", "Mar","Mier","Jue","Vier"];
+
 console.log(arrays_string)
+//operadores aritmaticos +,-,*,/ %}
 
+//Operadores de comparacion 
 
-    
-
-//estructura logica
-for (let i = 0; i < arrays_string.length; i++) {
+//Estructuras logicas
+//Inicializador 
+ for (let i = 0; i < arrays_string.length; i++) {
   console.log(arrays_string[i])
- Swal.fire("Bienvenidos ")
-}
-
-
-/*console.log(arrays_string)
-console.log(arrays_string.length)
-var impresion = "";
-for (let i = 0; i < arrays_string.length; i++){
-    console.log(arrays_string[i])
-impresion = impresion + arrays_string[i]
-}
-Swal.fire(impresion)
-*/
-
-
-var num1 = 14
-var num2 = 15
-var suma = num1 + num2
-console.log(suma)
+  Swal.fire("Bienvenidos");
+ }
